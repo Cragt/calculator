15d6252e4acf9bda779d15d6671d3d9123bcbf0a
Craig <cragt369@gmail.com>
2022-10-25 19:57:27 -0500

    Pressing numbered buttons almost displays the number inside the input field

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # calculator
+
 A simple calculator webapp using JavaScript, HTML, & CSS.
--- script.js
+++ script.js
@@ -4,15 +4,22 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 const operate = (a, operator, b) => {
-    if (operator === '+') return a + b;
-    if (operator === '-') return a - b;
-    if (operator === '*') return a * b;
-    if (operator === '/') return a / b;
+  if (operator === "+") return a + b;
+  if (operator === "-") return a - b;
+  if (operator === "*") return a * b;
+  if (operator === "/") return a / b;
+};
+
+let display = document.getElementById("display");
+
+const btn = document.getElementsByTagName("button");
+const numberOfButtons = btn.length;
+for (i = 0; i < numberOfButtons; i++) {
+  btn[i].addEventListener("click", myFunction);
 }
 
+function myFunction() {
+  // document.getElementById("display").value = btn[0].value;
+}
 
-console.log(operate(5, '/', 5))
-// console.log(add(9, 3))
-// console.log(subtract(9, 3))
-// console.log(multiply(9, 3))
-// console.log(divide(9, 3))
+// Make myFunction display the button's number in the input field, currently only works if I specify the specific index of btn
